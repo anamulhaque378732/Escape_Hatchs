9d992cfc4bf2c6999bd56be88465fd791c3a15b9
Ananul Haque <ananulhaque4631@gmail.com>
2026-01-05 11:48:00 +0600

    Life

--- Escape_Hatch/src/Components/ChatRoom.jsx
+++ Escape_Hatch/src/Components/ChatRoom.jsx
@@ -3,28 +3,23 @@ import createConnection from "../Utils/connection";
 
 const serverUrl = "https://localhost:1234";
 
-
-
 const ChatRoom = ({ roomId }) => {
-    useEffect(() => {
-        // synchronize with external chat server
+  useEffect(() => {
+    // synchronize with external chat server
 
-        const connection = createConnection(serverUrl, roomId);
-        connection.connect();
-        // cleanup
-        return () => {
-            connection.disconnect();
-        }
-    }, [roomId])
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    // cleanup
+    return () => {
+      connection.disconnect();
+    };
+  }, [roomId]);
 
-
-
-
-    return (
-        <div>
-            <h2> Welcome to the {roomId} room</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2> Welcome to the {roomId} room</h2>
+    </div>
+  );
 };
 
 export default ChatRoom;
